f0924d6bde869bbdeade1dfe14625efa7aa6c318
sonnth <sonnth@nal.vn>
2023-11-06 17:11:54 +0700

    Fix wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,13 +16,12 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        while db_up:
+        while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write("""Database unavaiable,
-                                   waitting for 1 second...""")
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database avaiable!'))
