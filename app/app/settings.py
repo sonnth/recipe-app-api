ae5d36f50898970d17994076c5ec81acc65d6b66
sonnth <sonnth@nal.vn>
2023-11-13 13:33:28 +0700

    Add unit test and User models

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-	'HOST': os.environ.get('DB_HOST'),
-	'NAME': os.environ.get('DB_NAME'),
-	'USER': os.environ.get('DB_USER'),
-	'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
